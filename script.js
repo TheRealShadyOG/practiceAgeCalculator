943f21c70e8b54a3f47cf98c770bb66d3f026b52
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-02-03 17:44:22 -0600

    Create variables for birthDate

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ document.getElementById('todaydate').innerHTML=todayDate;
 const birthDate = prompt("What is your Birthdate? DD-MM-YYYY");
 
 document.getElementById("yourbirthdate").innerHTML=birthDate;
+
+// Creating variables for birthdate
+let birthYear = Number(birthDate.slice(-4))
+let birthMonth = Number(birthDate.slice(3,5))
+let birthDay = Number(birthDate.slice(0,2))
+
